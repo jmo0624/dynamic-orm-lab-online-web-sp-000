ed2ea707983e5cdc0bbcbf55cc38c78d2c9658bb
Josh Morris <jmo0624@gmail.com>
2020-01-12 21:42:46 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -22,7 +22,7 @@ class InteractiveRecord
   end
   
   def initialize(options={})
-    option.each do |property, value|
+    options.each do |property, value|
       self.send("#{property}=", value)
     end
   end
