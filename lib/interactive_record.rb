8bf00658f395c2483edd0828b0021edf27f03990
Josh Morris <jmo0624@gmail.com>
2020-01-12 21:34:30 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,47 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+  
+  def self.column_names
+    DB[:conn].results_as_hash = true
+    
+    sql = "PRAGMA table_info('#{table_name}')"
+    
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+    column_names.compact
+  end
+  
+  def initialize(options={})
+    option.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+  
+  def table_name_for_insert
+    self.class.table_name
+  end
+  
+  def col_names_for_insert
+    self.class.column_names_delete_if {|col| col == "id"}.join(", ")
+  end
+  
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+  
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES"
+  
 end
